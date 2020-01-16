9b83e5db0765208b21461d6736ed3cceea1b3e24
dylans123 <dylanskelly@knights.ucf.edu>
2020-01-15 23:07:19 -0500

    Configuration for deployment to GCP

--- client/build/precache-manifest.3cb85228f59bc03e1b58a3e180788941.js
+++ client/build/precache-manifest.3cb85228f59bc03e1b58a3e180788941.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6ab56738ece56a7bdee1d4dd393cc675",
+    "revision": "652add5990185f51cd298e41a31a69ac",
     "url": "/index.html"
   },
   {
-    "revision": "7ea5389f183cc089359f",
+    "revision": "70209a47df0114f45fd4",
     "url": "/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "7ea5389f183cc089359f",
-    "url": "/static/js/2.8c83da29.chunk.js"
+    "revision": "70209a47df0114f45fd4",
+    "url": "/static/js/2.6bb4d318.chunk.js"
   },
   {
     "revision": "86afaa925e0f432774bf95b19b6933fb",
-    "url": "/static/js/2.8c83da29.chunk.js.LICENSE"
+    "url": "/static/js/2.6bb4d318.chunk.js.LICENSE"
   },
   {
-    "revision": "2c5d1ab2ac840aa88453",
-    "url": "/static/js/main.2df42a4e.chunk.js"
+    "revision": "38f57b0094f94ef3449a",
+    "url": "/static/js/main.910d2724.chunk.js"
   },
   {
     "revision": "65326020070271871721",
