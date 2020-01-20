e22473a8339dd92b8f6ed2b3fd2296372345aa86
Jerold Lodge <jlodge@knights.ucf.edu>
2020-01-19 21:19:40 -0500

    getContacts json

--- client/src/components/contactsList.js
+++ client/src/components/contactsList.js
@@ -9,30 +9,31 @@ export default class ContactsList extends Component {
         super(props);
         this.state = {
             name: '',
-            contactsarray: '',
+            contactsarray: [],
         }
     }
-    namsfe = 'asdrt';
+
     getContacts = () => {
         api.getContact().then(res => {
-            //console.log(res)
-            this.state.contactsarray = res.data
-            console.log(this.contactsarray)
+
+            this.setState({
+                name: '',
+                contactsarray: res.data,
+            })
+
+            console.log(this.state.contactsarray)
             
             if (!res.data.errmsg) {
                 console.log('Data fetched')
             } else {
                 console.log('Error fetching data')
             }
+        }).catch(function (error) {
+            console.log(error);
         })
     }
 
-    componentDidMount() {
-        this.getContacts();
-    }
-
     render() {
-        const { contacts } = this.state.contactsarray;
             return (
                 <div>
                     <Col >
@@ -57,16 +58,11 @@ export default class ContactsList extends Component {
                                 <td>111-1111</td>
                                 <td>motto@gmail.com</td>
                                 <td>
-                                    <button type="button" class="close" aria-label="Close">
+                                    <button type="button" className="close" aria-label="Close">
                                         <span aria-hidden="true">&times;</span>
                                     </button>
                                 </td>
                             </tr>
-                            {contacts.map(({ contact, key }) => (
-                                    <tr>
-                                    <td>{contact}</td>
-                                    </tr>
-                            ))}
                         </tbody>
                     </Table>
                 </div>
