2cfaffca2a1cc9527f907f51990d81bf565f57db
Jerold Lodge <jlodge@knights.ucf.edu>
2020-01-23 12:51:38 -0500

    getContacts() added

--- client/src/components/contactsList.js
+++ client/src/components/contactsList.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { Table, Input, Row, Col, Button } from 'reactstrap';
 import api from '../api'
-import axios from 'axios'
+import { MdEdit } from "react-icons/md";
+import { FaTimes } from "react-icons/fa";
+import { MdRefresh } from "react-icons/md";
+
+
 
 export default class ContactsList extends Component {
 
@@ -33,6 +37,27 @@ export default class ContactsList extends Component {
         })
     }
 
+    contactList() {
+        return this.state.contactsarray.map(function (currentContact, i) {
+            return (
+                <tr>
+                    <td>  {currentContact.name} </td>
+                    <td>  Lastnamefiller </td>
+                    <td>  111-1111 </td>
+                    <td>  email@gmail.com </td>
+                    <td>
+                        <button type="button" className="close" aria-label="Close">
+                            <span aria-hidden="true"><FaTimes /></span>
+                        </button>
+                        <button type="button" className="close" aria-label="Close">
+                            <span aria-hidden="true"><MdEdit /></span>
+                        </button>
+                    </td>
+                </tr>
+            );
+        })
+    }
+
     render() {
             return (
                 <div>
@@ -40,7 +65,7 @@ export default class ContactsList extends Component {
                         <Input type="firstname" name="firstname" id="contactFirst" onChange={(e) => this.updateName(e)} />
                     </Col>
                     <Button type="button" onClick={() => this.getContacts()}>
-                     Refresh
+                        <MdRefresh />
                     </Button>
                     <Table hover borderless responsive>
                         <thead>
@@ -63,6 +88,7 @@ export default class ContactsList extends Component {
                                     </button>
                                 </td>
                             </tr>
+                            {this.contactList()}
                         </tbody>
                     </Table>
                 </div>
