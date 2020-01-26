730842275fd831bb22f5dc9c9eac486003d11be7
Jerold Lodge <jlodge@knights.ucf.edu>
2020-01-26 18:03:00 -0500

    Ported to material UI

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,69 +1,128 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Input, Label, Col, Form, FormGroup, Jumbotron, Button } from 'reactstrap';
 import { Redirect } from 'react-router-dom'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-			email: '',
-			password: '',
-			redirectTo: null
-		}
+            email: '',
+            password: '',
+            redirectTo: null
+        }
         this.onSubmit = this.onSubmit.bind(this)
-		this.handleChange = this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.useStyles = this.useStyles.bind(this)
     }
 
     handleChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value
-		})
-	}
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
 
-	onSubmit(event) {
+    onSubmit(event) {
         event.preventDefault();
         const { email, password } = this.state;
         const { _login } = this.props;
         console.log('handleSubmit');
         _login(email, password);
-	}
+    }
+
+    useStyles = makeStyles(theme => ({
+        paper: {
+            marginTop: theme.spacing(8),
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+        avatar: {
+            margin: theme.spacing(1),
+            backgroundColor: theme.palette.secondary.main,
+        },
+        form: {
+            width: '100%', // Fix IE 11 issue.
+            marginTop: theme.spacing(1),
+        },
+        submit: {
+            margin: theme.spacing(3, 0, 2),
+        },
+    }));
 
     render() {
         const { redirectTo, email, password } = this.state;
 
+        const classes = this.useStyles;
+
         if (redirectTo) {
-			return <Redirect to={{ pathname: redirectTo }} />
+            return <Redirect to={{ pathname: redirectTo }} />
         }
 
-        return (
-            <div className="row justify-content-center align-items-center" style={{ height: '100vh' }}>
-                <div className="col-12 col-sm-6">
-                    <Jumbotron>
-                        <h1 className="display-3">Welcome Back!</h1>
-                        <p className="lead">Fill in your information below to login and view your contacts!</p>
-                        <hr className="my-2" />
-                        <Form onSubmit={this.onSubmit}>
-                            <FormGroup row>
-                                <Label for="userEmail" sm={3}>Email</Label>
-                                <Col sm={9}>
-                                    <Input type="email" name="email" id="userEmail" value={email} onChange={this.handleChange} />
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label for="userPassword" sm={3}>Password</Label>
-                                <Col sm={9}>
-                                    <Input type="password" name="password" id="userPassword" value={password} onChange={this.handleChange} />
-                                </Col>
-                            </FormGroup>
-                            <Button type="submit" size='lg' block>Login</Button>
-                        </Form>
-                        <hr className="my-2" />
-                        <p className="lead">Don't have an account yet? Register for one <Link to="/register">here.</Link></p>
-                    </Jumbotron>
-                </div>
+        return (<Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} onSubmit={this.onSubmit} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={this.handleChange}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={this.handleChange}
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Sign In
+          </Button>
+                    <Grid container>
+                        <Grid item xs>
+                        </Grid>
+                        <Grid item>
+                            <Link to="/register">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
             </div>
-        )
+        </Container>
+        );
     }
 }
 
